2c3df72298090a1b4c93b7f471801866d0fa514d
SYSTEM-MEMZ-XEK <XEK20090428@163.com>
2026-07-05 17:12:32 +0800

    feat: 对话框按钮圆角+弹性动画入场/退场

--- DialogOverlayWindow.xaml.cs
+++ DialogOverlayWindow.xaml.cs
@@ -12,11 +12,20 @@ namespace GaokaoCountdown
             Loaded += (_, _) =>
             {
                 Opacity = 0;
-                var fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(150))
+                var fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(200))
                 {
                     EasingFunction = new CubicEase { EasingMode = EasingMode.EaseOut }
                 };
+                var scaleIn = new DoubleAnimation(0.92, 1, TimeSpan.FromMilliseconds(250))
+                {
+                    EasingFunction = new BackEase { EasingMode = EasingMode.EaseOut, Amplitude = 0.3 }
+                };
                 BeginAnimation(OpacityProperty, fadeIn);
+                if (RootBorder.RenderTransform is ScaleTransform st)
+                {
+                    st.BeginAnimation(ScaleTransform.ScaleXProperty, scaleIn);
+                    st.BeginAnimation(ScaleTransform.ScaleYProperty, scaleIn);
+                }
                 if (ContentHost.Content is FrameworkElement fe) fe.Focus();
             };
         }
@@ -28,12 +37,21 @@ namespace GaokaoCountdown
 
         public void CloseWithFade()
         {
-            var fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(120))
+            var fadeOut = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(150))
+            {
+                EasingFunction = new CubicEase { EasingMode = EasingMode.EaseIn }
+            };
+            var scaleOut = new DoubleAnimation(1, 0.92, TimeSpan.FromMilliseconds(150))
             {
                 EasingFunction = new CubicEase { EasingMode = EasingMode.EaseIn }
             };
             fadeOut.Completed += (_, _) => Close();
             BeginAnimation(OpacityProperty, fadeOut);
+            if (RootBorder.RenderTransform is ScaleTransform st)
+            {
+                st.BeginAnimation(ScaleTransform.ScaleXProperty, scaleOut);
+                st.BeginAnimation(ScaleTransform.ScaleYProperty, scaleOut);
+            }
         }
     }
 }
